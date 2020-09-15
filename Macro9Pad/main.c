bed7a46eb4ee89f9f80e9c8315da9b9da5bed12a
Zack Littell <zlittell@gmail.com>
2020-09-15 13:50:28 -0500

    started Atmel Studio project, initial commit

---
+++ Macro9Pad/main.c
@@ -0,0 +1,21 @@
+/*
+ * Macro9Pad.c
+ *
+ * Created: 9/15/2020 1:43:22 PM
+ * Author : zlitt
+ */ 
+
+
+#include "sam.h"
+
+
+int main(void)
+{
+    /* Initialize the SAM system */
+    SystemInit();
+
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
