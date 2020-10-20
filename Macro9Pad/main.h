9a3bf1f8ea2d03e047e3fad24acd4ae1ee0dcf8f
Zachery Littell <zlittell@gmail.com>
2020-10-20 16:47:01 -0500

    default memory changed to example profile1 and filler profile 2

--- Macro9Pad/main.h
+++ Macro9Pad/main.h
@@ -12,6 +12,7 @@
 // this is a sample profile programmed initially
 uint8_t __attribute__ ((section (".deviceProfileSection"))) initProfile[64] = 
 {
+	//Default Save 1
 	// 32bytes 1
 	0x11,	// RGB RED
 	0x22,	// RGB Green
@@ -40,47 +41,9 @@ uint8_t __attribute__ ((section (".deviceProfileSection"))) initProfile[64] =
 	// 32bytes 6
 	0x00,	// Button9 noMod
 	0x13,	// Button9 p
-	//0x00,	// WriteCounterA
-	//0x00,	// WriteCounterB
-	0x4E,
-	0x1e,
-	
-	// 32bytes 1a
-	0x44,	// RGB RED
-	0x55,	// RGB Green
-	0x66,	// RGB Blue
-	0xFF,	// RGB Brightness
-	// 32bytes 2
-	0x02,	// Button1 LShift
-	0x1e,	// Button1 z
-	0x00,	// Button2 noMod
-	0x10,	// Button2 a
-	// 32bytes 3
-	0x00,	// Button3 noMod
-	0x06,	// Button3 c
-	0x00,	// Button4 noMod
-	0x0e,	// Button4 k
-	// 32bytes 4
-	0x02,	// Button5 LShift
-	0x1d,	// Button5 l
-	0x00,	// Button6 noMod
-	0x04,	// Button6 m
-	// 32bytes 5
-	0x00,	// Button7 noMod
-	0x11,	// Button7 n
-	0x00,	// Button8 noMod
-	0x12,	// Button8 o
-	// 32bytes 6
-	0x00,	// Button9 noMod
-	0x13,	// Button9 p
-	//0x00,	// WriteCounterA
-	//0x00,	// WriteCounterB
-	0x00,
-	0x00,
-	
-	
-	//fill
-	/*
+	0x00,	// WriteCounterA
+	0x00,	// WriteCounterB
+	// Filler for Save 2
 	// 32bytes 7
 	0x00,0x00,0x00,0x00,
 	// 32bytes 8
@@ -101,7 +64,6 @@ uint8_t __attribute__ ((section (".deviceProfileSection"))) initProfile[64] =
 	0x00,0x00,0x00,0x00,
 	// 32bytes 16
 	0x00,0x00,0x00,0x00
-	*/
 };
 
 
