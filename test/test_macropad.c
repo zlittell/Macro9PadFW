14868bce913246df8cf800bad364ac57b229f5a9
Zachery Littell <zlittell@gmail.com>
2020-12-23 12:31:26 -0600

    fix mock issues with test_macropad.c

--- test/test_macropad.c
+++ test/test_macropad.c
@@ -1,6 +1,12 @@
 #include "unity.h"
 
 #include "macropad.h"
+#include "version.h" // Include the device version for processing
+#include "serialnumber.h" // Include the default serial number
+
+#include "mock_MSF_RGB.h"
+#include "mock_MSF_NVM_FLASH.h"
+#include "mock_sys_samd11.h"
 
 void setUp(void)
 {
@@ -8,9 +14,4 @@ void setUp(void)
 
 void tearDown(void)
 {
-}
-
-void test_macropad_NeedToImplement(void)
-{
-    TEST_IGNORE_MESSAGE("Need to Implement macropad");
 }
